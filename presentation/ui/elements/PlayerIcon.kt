d70e82a3f11ad65973a78901c57e35d37dca7c00
ValentinKDev <98110864+ValentinKDev@users.noreply.github.com>
2022-03-16 12:18:45 +0100

    Player Icon in Multiple Directions

--- presentation/ui/elements/PlayerIcon.kt
+++ presentation/ui/elements/PlayerIcon.kt
@@ -2,6 +2,7 @@ package com.mobilegame.robozzle.presentation.ui.elements
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.GenericShape
 import androidx.compose.runtime.Composable
@@ -10,90 +11,133 @@ import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.rotationMatrix
+import com.mobilegame.robozzle.domain.InGame.Direction
 import com.mobilegame.robozzle.presentation.res.*
 import com.mobilegame.robozzle.presentation.ui.utils.CenterComposable
+import com.mobilegame.robozzle.utils.Extensions.toInt
 
 @Preview
 @Composable
 fun TestIt() {
-    PlayerIcon(widhtDp = 100)
+    PlayerIcon(widhtDp = 100, Direction(1,0))
 }
 
 @Composable
-fun PlayerIcon(widhtDp: Int) {
-   Box(modifier = Modifier
-       .size(widhtDp.dp)
-   ){
-       CenterComposable {
-           Box(modifier = Modifier
-               .size((0.9F * widhtDp ).dp)
-               .drawBehind {
-                   val height = size.height
-                   val width = size.width
+fun PlayerIcon(widhtDp: Int, direction: Direction) {
+    Box(modifier = Modifier
+        .size(widhtDp.dp)
+    ){
+        CenterComposable {
+            androidx.compose.foundation.Canvas(modifier = Modifier.size((0.9F * widhtDp).dp)) {
+                val armorColor = Color.Black
+                val cap = StrokeCap.Round
+                val strokeWidth = 6F
 
-                   val start = 0F
-                   val end = width
-                   val top = 0F
-                   val bottom = height
+                val height = size.height
+                val width = size.width
 
-                   val yCenter = (bottom - top) / 2F
+                val start = 0F
+                val end = width
+                val top = 0F
+                val bottom = height
 
-                   val trianglePath = Path().apply {
-                       moveTo(start, 0.07F * end)
-                       lineTo(width, yCenter)
-                       lineTo(start, 0.93F * end)
-                       lineTo(width / 5F, yCenter)
-                   }
-                   drawPath(trianglePath, Color(0xFFEE6615))
+                val yCenter = (bottom - top) / 2F
 
-                   val armorColor = yellowDark2
-                   val cap = StrokeCap.Round
-                   val strokeWidth = 6F
-                   drawLine(
-                       start = Offset(width / 5F, yCenter),
-                       end = Offset(end, yCenter),
-                       color = armorColor,
-                       cap = cap,
-                       strokeWidth = strokeWidth
-                   )
+                val trianglePathLeftHalf = Path().apply {
+                    moveTo(start, 0.07F * end)
+                    lineTo(width, yCenter)
+                    lineTo(width / 5F, yCenter)
+                }
+                val trianglePathRightHalf = Path().apply {
+//                       moveTo(start, 0.07F * end)
+                    moveTo(width, yCenter)
+                    lineTo(start, 0.93F * end)
+                    lineTo(width / 5F, yCenter)
+                }
 
-                   drawLine(
-                       start = Offset(width, yCenter),
-                       end = Offset(start, 0.93F * bottom),
-                       color = armorColor,
-                       cap = cap,
+                rotate(
+                    pivot = center,
+                    degrees = when (direction.ToChar()) {
+                        'u' -> 90F
+                        'd' -> 270F
+                        'r' -> 0F
+                        'l' -> 180F
+                        else -> 45F
+                    }
+                ) {
+                    drawPath(
+                        trianglePathLeftHalf,
+                        brush = Brush.verticalGradient(
+                            listOf(
+                                Color(0xFFA23807),
+                                Color(0xFFDD5412),
+                                Color(0xFFEE6615),
+                            )
+                        )
+                    )
+                    drawPath(
+                        trianglePathRightHalf,
+                        brush = Brush.verticalGradient(
+                            listOf(
+                                Color(0xFFEE6615),
+                                Color(0xFFDD5412),
+                                Color(0xFFBD4409),
+                                Color(0xFFA23807),
+                            )
+                        )
+                    )
+
+                    drawLine(
+                        start = Offset(width / 5F, yCenter),
+                        end = Offset(end, yCenter),
+                        color = armorColor,
+                        cap = cap,
+                        strokeWidth = 4F
+                    )
+                    //ext rigth
+                    drawLine(
+                        start = Offset(width, yCenter),
+                        end = Offset(start, 0.93F * bottom),
+                        color = armorColor,
+                        cap = cap,
 //                       strokeWidth = strokeWidth,
-                       strokeWidth = 5F,
-                   )
-                   drawLine(
-                       start = Offset(start, 0.93F * bottom),
-                       end = Offset(width / 5F, yCenter),
-                       color = armorColor,
-                       cap = cap,
-//                       strokeWidth = strokeWidth,
-                       strokeWidth = 5F,
-                   )
+                        strokeWidth = 5F,
+                    )
+                    //ext left
+                    drawLine(
+                        start = Offset(x = start, y = 0.07F * end),
+                        end = Offset(width, yCenter),
+                        color = armorColor,
+                        cap = cap,
+                        strokeWidth = 5F,
+                    )
 
-                   drawLine(
-                       start = Offset(x = start, y = 0.07F * end),
-                       end = Offset(width, yCenter),
-                       color = armorColor,
-                       cap = cap,
-                       strokeWidth = strokeWidth
-                   )
-                   drawLine(
-                       start = Offset(width / 5F, yCenter),
-                       end = Offset(x = start, y = 0.07F * end),
-                       color = armorColor,
-                       cap = cap,
-                       strokeWidth = strokeWidth
-                   )
-               }
-           )
+                    //bottom right
+                    drawLine(
+                        start = Offset(width / 5F, yCenter),
+                        end = Offset(start, 0.93F * bottom),
+                        color = armorColor,
+                        cap = cap,
+                        strokeWidth = strokeWidth,
+                    )
+                    //bottom left
+                    drawLine(
+                        start = Offset(width / 5F, yCenter),
+                        end = Offset(x = start, y = 0.07F * end),
+                        color = armorColor,
+                        cap = cap,
+                        strokeWidth = strokeWidth
+                    )
+
+                }
+            }
        }
    }
 }
